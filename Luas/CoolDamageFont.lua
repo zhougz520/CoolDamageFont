37e0b57eb58d64a3cf024fc3151d0eae6c03081a
周冠舟 <zhougza@dcclouds.com>
2024-07-31 16:28:19 +0800

    init

---
+++ Luas/CoolDamageFont.lua
@@ -0,0 +1,21 @@
+-- Shows addon loaded in chat window
+print("CoolDamageFont v1.0")
+
+-- Locals
+local damagefont_FONT_NUMBER = "Interface\\AddOns\\CoolDamageFont\\font.ttf"
+
+-- Code Base
+CoolDamageFont = CreateFrame("Frame", "CoolDamageFont")
+
+function CoolDamageFont:ApplySystemFonts()
+	DAMAGE_TEXT_FONT = damagefont_FONT_NUMBER
+end
+
+CoolDamageFont:SetScript("OnEvent", function()
+	if event == "ADDON_LOADED" then
+		CoolDamageFont:ApplySystemFonts()
+	end
+end)
+
+CoolDamageFont:RegisterEvent("ADDON_LOADED")
+CoolDamageFont:ApplySystemFonts()
